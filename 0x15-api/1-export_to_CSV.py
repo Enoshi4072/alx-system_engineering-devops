9e7b8d6a8fb84469e4eb2bfbdb4eebb5c30dbcd5
Enoshi4072 <enockkibet6642@gmail.com>
2023-10-23 21:21:37 +0000

    1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""  Python script to export data in the CSV format.
-
-Requirements:
-
-Records all tasks that are owned by this employee
-Format must be: "USER_ID","USERNAME",
-"TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv """
 import requests
 import sys
 import csv
@@ -19,26 +11,20 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos".format(base_url)
 
     user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Employee not found or API request failed.")
-        sys.exit(1)
+    todos_response = requests.get(todos_url, params={"userId": employee_id})
 
     user_data = user_response.json()
     todos_data = todos_response.json()
-
     employee_name = user_data.get('name')
-    completed_tasks = [task for task in todos_data if task['completed']]
 
-    # Create a CSV file for the user
-    filename = f"{employee_id}.csv"
+    # Create a CSV file for the user with values enclosed in double quotes
+    filename = "{}.csv".format(employee_id)
     with open(filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todos_data:
-            csv_writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [csv_writer.writerow(
+            [str(employee_id), employee_name, str(
+                task['completed']), task['title']]) for task in todos_data]
