c72bf7f5e73ed72ddc2042555ae26858ec065a83
Enoshi4072 <enockkibet6642@gmail.com>
2023-10-23 21:52:49 +0000

    2. Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,43 +1,24 @@
 #!/usr/bin/python3
-"""
-Python script to export data in the JSON format.
-
-Requirements:
-
-Records all tasks that are owned by this employee
-"""
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
 import requests
 import sys
-import json
 
-if __name__ == "__main":
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com'
-
-    # Fetch user information
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    user_url = f"{base_url}/users/{user_id}"
+    user_response = requests.get(user_url)
     user_data = user_response.json()
-    username = user_data.get('username')
+    username = user_data.get("username")
+    
+    todos_url = f"{base_url}/todos"
+    todos_response = requests.get(todos_url, params={"userId": user_id})
+    todos_data = todos_response.json()
 
-    # Fetch user's TODO list
-    todo_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-    todo_data = todo_response.json()
+    json_data = {user_id: [
+        {"task": t.get("title"), "completed": t.get("completed"), "username": username} for t in todos_data]}
 
-    # Create a list of tasks
-    tasks = []
-    for task in todo_data:
-        task_info = {
-            'task': task.get('title'),
-            'completed': task.get('completed'),
-            'username': username
-        }
-        tasks.append(task_info)
-
-    # Create a JSON file with the user's tasks
-    file_name = f'{employee_id}.json'
-    with open(file_name, 'w') as json_file:
-        json.dump({employee_id: tasks}, json_file)
+    with open(f"{user_id}.json", "w") as jsonfile:
+        json.dump(json_data, jsonfile)
